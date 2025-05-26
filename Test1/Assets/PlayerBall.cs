e1f0d90c97207b8ec9439a4e2a87a609714639b3
PcilA <charlottekrgy@gmail.com>
2025-05-26 21:06:32 +0900

    유니티 기본파일 및 구체이동기능

---
+++ Test1/Assets/PlayerBall.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+
+    Rigidbody rigid;
+
+    void Awake()
+    {  rigid = GetComponent<Rigidbody>(); }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+
+
+}
